0691af179693703707748b6f64f227b53b03b63b
Jyothi333 <87903571+Jyothi333@users.noreply.github.com>
2022-03-08 17:15:34 +0530

    Add files via upload

---
+++ Decrement.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class Decrement {
+
+	public static void main(String[] args) {
+	int a=10;
+	a--;	
+		System.out.println(a);
+		
+
+	}
+
+}
---
+++ Increment.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class Increment {
+
+	public Increment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int a=33;
+		a++;
+		System.out.print(a);
+		 
+
+	}
+
+}

1a5427f5b5dc17633c82816ccbd7dbf4097ccfc8
Undela Jyothi <87903571+Jyothi333@users.noreply.github.com>
2022-03-23 11:20:06 +0530

    JS

---
+++ Javascript/obj-ex.js
@@ -0,0 +1,4 @@
+var obj1={name :"Jyothi",Age : 22, role :"ASE"};
+var obj2={name : "Prashu",Age : 22,role :"Jyo_Bff"};
+console.log(obj1.name);
+console.log(obj2.Age);
---
+++ Javascript/var_declar.js
@@ -0,0 +1,9 @@
+var a = 10;
+//let, const
+var c =" hello " ;
+var d = ' team ' ;
+var e = true;
+console.log(typeof(e));
+var empty = null;
+var nan = NaN;   // number data type
+var notDefined = undefined;  //undefined data type
